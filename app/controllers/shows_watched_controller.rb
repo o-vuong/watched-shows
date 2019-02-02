e8bda33208ff2634f88594dcc7b5c281062d3460
Oscar Vuong <oscarvuong@gmail.com>
2019-02-02 12:11:44 -0800

    shows.html renamed to index.html created new shows.html cleaned up duplicate codes

--- app/controllers/shows_watched_controller.rb
+++ app/controllers/shows_watched_controller.rb
@@ -7,7 +7,7 @@ class ShowsWatchedController < ApplicationController
       @shows = current_user.show.build(params)
       @shows = Show.all
      
-      erb :'/shows/shows.html'
+      erb :'/shows/index.html'
     end
   end
 
@@ -31,74 +31,12 @@ class ShowsWatchedController < ApplicationController
   end
   
 
-
-
-  get '/shows/:id/edit' do
-     @shows = Show.find_by_id(params[:id])
-        if !logged_in?
-        redirect '/login'
-        else
-          if @shows && @shows.user == current_user
-            else
-            erb :'/shows/edit.html'
-          end
-        end
-  end
-
-  post "/shows/:id" do 
-    if !logged_in?
-      redirect '/login'
-    else
-      if @shows && @shows.user == current_user
-        if @shows.update(title: params[:title], date: params[:date])
-          redirect "/shows/#{@shows.id}"
-        else
-          "/shows/#{@shows.id}/edit"
-        end
-      else
-        redirect '/shows'
-      end
-
-    end
-  end
-
-
-
-
-  patch '/shows/:id' do
-    if logged_in?
-      if params[:title] == "" 
-        redirect to "/shows/#{params[:id]}/edit"
-      else
-        @shows = Show.find_by_id(params[:id])
-        if @shows && @shows.user == current_user
-          if @shows.update(title: params[:title], date: params[:date])
-            redirect to "/shows/#{@shows.id}"
-          else 
-            redirect to "/shows/#{@shows.id}/edit"
-          end
-        else 
-          redirect to '/shows'
-        end
-      end
-    else 
-      redirect to '/login'
-    end
-  end
-
-  get '/shows/:id' do
-    if logged_in?
-      @shows = Show.find_by_id(params[:id])
-      erb :"shows/shows.html"
-    else
-      redirect to '/login'
-    end
-  end
-
   get '/shows/:id/edit' do
     if logged_in?
-      @shows = Show.find_by_id(params[:id])
+      @error_message = params[:error]
+      @shows = Show.find_by_id(params[:id]) 
       if @shows && @shows.user == current_user
+         @error_message = params[:error]
         erb :'shows/edit.html'
       else
         redirect to '/shows'
@@ -108,13 +46,33 @@ class ShowsWatchedController < ApplicationController
     end
   end
 
+  patch '/shows/:id' do
+    @shows = Show.find_by_id(params[:id])
+    unless Show.validators(params[:id])
+      redirect "/shows/#{@shows.id}/edit?error=invalid show"
+    end
+    @shows.update(params.select{|k|k=="title" || k=="date" || k=="user_id"})
+    redirect "/shows/#{@shows.id}"
+  end
 
-  
-  delete '/shows/:id/delete' do
+  get '/shows/:id/delete' do
     if logged_in?
       @shows = Show.find_by_id(params[:id])
       if @shows && @shows.user == current_user
-        @shows.delete
+      else
+      redirect '/shows'
+      end
+    else
+      redirect '/login'
+    end
+  end
+  
+  delete '/shows/:id' do
+    if logged_in?
+      @shows = Show.find_by_id(params[:id])
+      if @shows && @shows.user == current_user
+        binding.pry
+        @shows.destroy(params[:id])
       end
       redirect to '/shows'
     else
